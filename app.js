aa55ba4c8842ea48fe344da3092b5f74f27428c3
phbord <phbord@gmail.com>
2023-09-16 01:02:03 +0200

    app.js update

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 const path = require('path');
 const bookRoutes = require('./routes/book');
 const userRoutes = require('./routes/user');
@@ -8,17 +7,11 @@ const utilsConnect = require('./utils/connect');
 require('dotenv').config();
 
 const app = express();
-const uri = `${process.env.MONGODB_ROOT}${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}${process.env.MONGODB_URI_PART}?retryWrites=true&w=majority`;
 
 // Accès au corps de la requête
 app.use(express.json());
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(e => console.log(`Connexion à MongoDB échouée ! - ${e}`)
-);
-
-//utilsConnect.mongooseConnect();
+utilsConnect.mongooseConnect();
 
 // Appliqué à toutes les routes
 app.use((req, res, next) => {
